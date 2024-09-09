9533f30822fec8509936d773c3175c52e15a5e4c
mohamed_7 <moodo1999712@gmail.com>
2024-09-09 11:36:26 +0300

    solve double count if click multible times in same box

--- script.js
+++ script.js
@@ -3,10 +3,17 @@ let x = []
 let y = []
 let count = 0
 let count1 = 0
+let bombNo = 10
+let hidenBox = 100
+let number = 0
 for (let i = 0; i < 100; i++) {
   x[i] = 0
   y[i] = '-'
 }
+const hiden = document.querySelector('hiden')
+const bomb1 = document.querySelector('bomb')
+hiden.innerText = hidenBox
+bomb1.innerText = bombNo
 const div = document.querySelectorAll('div')
 for (let l = 0; l < div.length; l++) {
   div[l].innerText = y[l]
@@ -14,23 +21,22 @@ for (let l = 0; l < div.length; l++) {
 for (let l = 0; l < div.length; l++) {
   div[l].addEventListener('click', () => {
     for (let l = 0; l < div.length; l++) {
-      count1 = l
       if (count == 0) {
         count++
-        for (let i = 0; i < 10; i++) {
+        for (let i = 0; i < bombNo; i++) {
           bomb[i] = Math.floor(Math.random() * 100)
-          while (bomb[i] == count1) {
+          while (x[bomb[i]] == div[l]) {
             bomb[i] = Math.floor(Math.random() * 100)
           }
         }
         for (let i = 0; i < bomb.length; i++) {
-          for (let j = i + 1; j < bomb.length; j++) {
-            while (bomb[i] === bomb[i + j] || bomb[i + j] == count1) {
+          for (let j = i + 1; j < bombNo; j++) {
+            while (bomb[i] === bomb[i + j] || x[bomb[i]] == div[l]) {
               bomb[i + j] = Math.floor(Math.random() * 100)
             }
           }
         }
-        for (let i = 0; i < bomb.length; i++) {
+        for (let i = 0; i < bombNo; i++) {
           x[bomb[i]] = '*'
           if (bomb[i] > 9) {
             if (bomb[i] % 10 > 0 && x[bomb[i] - 11] != '*') {
@@ -64,15 +70,73 @@ for (let l = 0; l < div.length; l++) {
       }
     }
     div[l].innerText = x[l]
+    if (x[l] !== y[l]) {
+      hidenBox--
+      y[l] = x[l]
+    }
+
+    if (x[l] == 0) {
+      if (l > 9) {
+        if (l % 10 > 0 && y[l - 11] !== x[l - 11]) {
+          div[l - 11].innerText = x[l - 11]
+          y[l - 11] = x[l - 11]
+          hidenBox--
+        }
+        if (y[l - 10] !== x[l - 10]) {
+          div[l - 10].innerText = x[l - 10]
+          y[l - 10] = x[l - 10]
+          hidenBox--
+        }
+        if (l % 10 < 9 && y[l - 9] !== x[l - 9]) {
+          div[l - 9].innerText = x[l - 9]
+          y[l - 9] = x[l - 9]
+          hidenBox--
+        }
+      }
+      if (l % 10 > 0 && y[l - 1] !== x[l - 1]) {
+        div[l - 1].innerText = x[l - 1]
+        y[l - 1] = x[l - 1]
+        hidenBox--
+      }
+      if (l % 10 < 9 && y[l + 1] !== x[l + 1]) {
+        div[l + 1].innerText = x[l + 1]
+        y[l + 1] = x[l + 1]
+        hidenBox--
+      }
+      if (l < 90) {
+        if (l % 10 > 0 && y[l + 9] !== x[l + 9]) {
+          div[l + 9].innerText = x[l + 9]
+          y[l + 9] = x[l + 9]
+          hidenBox--
+        }
+        if (y[l + 10] !== x[l + 10]) {
+          div[l + 10].innerText = x[l + 10]
+          y[l + 10] = x[l + 10]
+          hidenBox--
+        }
+        if (l % 10 < 9 && y[l + 11] !== x[l + 11]) {
+          div[l + 11].innerText = x[l + 11]
+          y[l + 11] = x[l + 11]
+          hidenBox--
+        }
+      }
+    }
+
+    hiden.innerText = hidenBox
+    bomb1.innerText = bombNo
+
     if (x[l] === '*') {
-      alert('You lost')
+      alert('You Lost')
+    } else if (hidenBox == bombNo) {
+      alert('You Win')
     }
   })
 }
+
 // const column = document.querySelectorAll('.column')
 //   for (let i = 0; i < 7; i++) {
 //     column[i].addEventListener('click', () => {
-//       let number = column[i].getAttribute('value')
+//       let number = div[l].getAttribute()
 //       console.log(number)
 //       const div = document.querySelectorAll('div')
 
