faf0168d9028d0cdea91ce58773d48ef900b96f8
mohamed_7 <moodo1999712@gmail.com>
2024-09-10 09:28:50 +0300

    i make win function

--- script.js
+++ script.js
@@ -8,20 +8,32 @@ let hidenBox = 100
 let number = 0
 let s = 0
 let m = 0
+
+const hiden = document.querySelector('hiden')
+const bomb1 = document.querySelector('bomb')
+const div = document.querySelectorAll('div')
+
+hiden.innerText = hidenBox
+bomb1.innerText = bombNo
+
 for (let i = 0; i < 100; i++) {
   x[i] = 0
   y[i] = '-'
 }
-const hiden = document.querySelector('hiden')
-const bomb1 = document.querySelector('bomb')
-hiden.innerText = hidenBox
-bomb1.innerText = bombNo
-const div = document.querySelectorAll('div')
+
 for (let l = 0; l < div.length; l++) {
   div[l].innerText = y[l]
 }
+
 for (let l = 0; l < div.length; l++) {
   div[l].addEventListener('click', () => {
+    function win() {
+      if (x[l] === '*') {
+        alert('You Lost')
+      } else if (hidenBox == bombNo) {
+        alert('You Win')
+      }
+    }
     for (let l = 0; l < div.length; l++) {
       if (count == 0) {
         count++
@@ -137,101 +149,7 @@ for (let l = 0; l < div.length; l++) {
       }
     }
     m = 0
-    // if (x[l] == 0) {
-    //   if (l > 9) {
-    //     if (l % 10 > 0 && y[l - 11] !== x[l - 11]) {
-    //       div[l - 11].innerText = x[l - 11]
-    //       y[l - 11] = x[l - 11]
-    //       hidenBox--
-    //     }
-    //     if (y[l - 10] !== x[l - 10]) {
-    //       div[l - 10].innerText = x[l - 10]
-    //       y[l - 10] = x[l - 10]
-    //       hidenBox--
-    //     }
-    //     if (l % 10 < 9 && y[l - 9] !== x[l - 9]) {
-    //       div[l - 9].innerText = x[l - 9]
-    //       y[l - 9] = x[l - 9]
-    //       hidenBox--
-    //     }
-    //   }
-    //   if (l % 10 > 0 && y[l - 1] !== x[l - 1]) {
-    //     div[l - 1].innerText = x[l - 1]
-    //     y[l - 1] = x[l - 1]
-    //     hidenBox--
-    //   }
-    //   if (l % 10 < 9 && y[l + 1] !== x[l + 1]) {
-    //     div[l + 1].innerText = x[l + 1]
-    //     y[l + 1] = x[l + 1]
-    //     hidenBox--
-    //   }
-    //   if (l < 90) {
-    //     if (l % 10 > 0 && y[l + 9] !== x[l + 9]) {
-    //       div[l + 9].innerText = x[l + 9]
-    //       y[l + 9] = x[l + 9]
-    //       hidenBox--
-    //     }
-    //     if (y[l + 10] !== x[l + 10]) {
-    //       div[l + 10].innerText = x[l + 10]
-    //       y[l + 10] = x[l + 10]
-    //       hidenBox--
-    //     }
-    //     if (l % 10 < 9 && y[l + 11] !== x[l + 11]) {
-    //       div[l + 11].innerText = x[l + 11]
-    //       y[l + 11] = x[l + 11]
-    //       hidenBox--
-    //     }
-    //   }
-    // }
-
     hiden.innerText = hidenBox
-
-    if (x[l] === '*') {
-      alert('You Lost')
-    } else if (hidenBox == bombNo) {
-      alert('You Win')
-    }
+    win()
   })
 }
-
-// const column = document.querySelectorAll('.column')
-//   for (let i = 0; i < 7; i++) {
-//     column[i].addEventListener('click', () => {
-//       let number = div[l].getAttribute()
-//       console.log(number)
-//       const div = document.querySelectorAll('div')
-
-console.log(bomb)
-
-// for (let i = 0; i < bomb.length; i++) {
-//   x[bomb[i]] = '*'
-//   if (bomb[i] > 9) {
-//     if (bomb[i] % 10 > 0 && x[bomb[i] - 11] != '*') {
-//       x[bomb[i] - 11] += 1
-//     }
-//     if (x[bomb[i] - 10] != '*') {
-//       x[bomb[i] - 10] += 1
-//     }
-//     if (bomb[i] % 10 < 9 && x[bomb[i] - 9] != '*') {
-//       x[bomb[i] - 9] += 1
-//     }
-//   }
-//   if (bomb[i] % 10 > 0 && x[bomb[i] - 1] != '*') {
-//     x[bomb[i] - 1] += 1
-//   }
-//   if (bomb[i] % 10 < 9 && x[bomb[i] + 1] != '*') {
-//     x[bomb[i] + 1] += 1
-//   }
-//   if (bomb[i] < 90) {
-//     if (bomb[i] % 10 > 0 && x[bomb[i] + 9] != '*') {
-//       x[bomb[i] + 9] += 1
-//     }
-//     if (x[bomb[i] + 10] != '*') {
-//       x[bomb[i] + 10] += 1
-//     }
-//     if (bomb[i] % 10 < 9 && x[bomb[i] + 11] != '*') {
-//       x[bomb[i] + 11] += 1
-//     }
-//   }
-// }
-console.log(x)
